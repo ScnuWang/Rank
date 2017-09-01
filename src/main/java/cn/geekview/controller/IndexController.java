7b2ccf33b8cb1ebad303dc6a4ac677918299aa8d
Jason <scnu_wang@163.com>
2017-09-01 18:44:10 +0800

    Revert "添加全局异常处理"
    This reverts commit 04603db

--- src/main/java/cn/geekview/controller/IndexController.java
+++ src/main/java/cn/geekview/controller/IndexController.java
@@ -29,6 +29,11 @@ public class IndexController {
         return "index";
     }
 
+    @RequestMapping("/errortest")
+    public String errortest(){
+        return "error";
+    }
+
     /**
      * 重定向到指定地址
      * @param response
